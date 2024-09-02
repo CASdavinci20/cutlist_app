a455c19773faf30a22e4f4382b68659522f7a1c6
Young Batimehin <benny.bat51@gmail.com>
2024-09-02 11:14:16 +0100

    l

---
+++ lib/base_page.dart
@@ -0,0 +1,91 @@
+import 'package:cutlist/notifications/notificationpage.dart';
+import 'package:flutter/material.dart';
+
+import 'home/homepage.dart';
+import 'mylist/mylistpage.dart';
+import 'profile/profilepage.dart';
+
+class BasePage extends StatefulWidget {
+  const BasePage({Key? key}) : super(key: key);
+
+  @override
+  State<BasePage> createState() => _BasePageState();
+}
+
+class _BasePageState extends State<BasePage> {
+  final PageController pageController = PageController(initialPage: 0);
+  late int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFEf1f1fc),
+      extendBody: true,
+      body: PageView(
+        controller: pageController,
+        children: const <Widget>[
+          Center(
+            child: HomePage(),
+          ),
+          Center(
+            child: MyListPage(),
+          ),
+          Center(
+            child: NotificationPage(),
+          ),
+          Center(
+            child: ProfilePage(),
+          ),
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.orange,
+        onPressed: () {},
+        shape: CircleBorder(),
+        child: const Icon(Icons.add),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(),
+        notchMargin: 5.0,
+        elevation: 0.0,
+        color: Colors.white,
+        child: SizedBox(
+          height: kBottomNavigationBarHeight,
+          child: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            backgroundColor: Colors.white,
+            elevation: 0.0,
+            currentIndex: _selectedIndex,
+            selectedItemColor: Colors.orange,
+            unselectedItemColor: Colors.grey,
+            onTap: (index) {
+              setState(() {
+                _selectedIndex = index;
+                pageController.jumpToPage(index);
+              });
+            },
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home_outlined),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.search),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.favorite_border_outlined),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.account_circle_outlined),
+                label: '',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/login/verificationpage.dart
+++ lib/login/verificationpage.dart
@@ -1,18 +1,17 @@
-import 'package:cutlist/home/homepage.dart';
 import 'package:cutlist/login/container/verificationInput.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
 
+import '../base_page.dart';
 
 class VerificationPage extends StatefulWidget {
-  const VerificationPage ({super.key});
-
+  const VerificationPage({super.key});
 
   @override
-  VerificationPageState createState()=> VerificationPageState();
+  VerificationPageState createState() => VerificationPageState();
 }
 
-class VerificationPageState  extends State<VerificationPage>{
+class VerificationPageState extends State<VerificationPage> {
   final TextEditingController _number1 = TextEditingController();
   final TextEditingController _number2 = TextEditingController();
   final TextEditingController _number3 = TextEditingController();
@@ -20,135 +19,116 @@ class VerificationPageState  extends State<VerificationPage>{
   final TextEditingController _number5 = TextEditingController();
   final TextEditingController _number6 = TextEditingController();
 
-  final VerificationInput verificationInput =VerificationInput();
+  final VerificationInput verificationInput = VerificationInput();
 
-
-  @override 
-    Widget build(BuildContextcontext){
-      return Scaffold(
-        backgroundColor: const Color(0xFFEf9f9f9),
-        body: Padding
-        (padding: EdgeInsets.fromLTRB(30, 70,30,30),
+  @override
+  Widget build(BuildContextcontext) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFEf9f9f9),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 70, 30, 30),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-          const  Text(
+            const Text(
               'Verify - +234-903-4344-435',
-                style: TextStyle(
-                 color:  Color(0xFFE0f2851),
-                   fontSize: 22,
-                 fontWeight: FontWeight.w500,
-             ),
+              style: TextStyle(
+                color: Color(0xFFE0f2851),
+                fontSize: 22,
+                fontWeight: FontWeight.w500,
+              ),
             ),
-            const SizedBox(height: 10,),
-
-           const Text(
+            const SizedBox(
+              height: 10,
+            ),
+            const Text(
               'Copy the OTP code sent to +234-903-4344-435.',
               textAlign: TextAlign.left,
-               style: TextStyle(
-                      color:  Color(0xFFE0333333),
-                      fontSize: 12,
-                      fontWeight: FontWeight.w500,
-                    ), 
+              style: TextStyle(
+                color: Color(0xFFE0333333),
+                fontSize: 12,
+                fontWeight: FontWeight.w500,
+              ),
             ),
-
-          const SizedBox(height: 10,),
-
+            const SizedBox(
+              height: 10,
+            ),
             GestureDetector(
-           child:const  Text(
+                child: const Text(
               'Wrong number ? ',
-               style: TextStyle(
-                      color:  Color(0xFFE0f3cd5b),
-                      fontSize: 14,
-                      fontWeight: FontWeight.w500,
-                    ), 
-           )
+              style: TextStyle(
+                color: Color(0xFFE0f3cd5b),
+                fontSize: 14,
+                fontWeight: FontWeight.w500,
+              ),
+            )),
+            const SizedBox(
+              height: 30,
             ),
-
-            const SizedBox(height: 30,),
-
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-              verificationInput.verificationInput(
-               inputNumber:  _number1
-              ),
-               verificationInput.verificationInput(
-               inputNumber:  _number2
-              ),
-               verificationInput.verificationInput(
-               inputNumber:  _number3
-              ),
-                verificationInput.verificationInput(
-               inputNumber:  _number4
-              ),
-               verificationInput.verificationInput(
-               inputNumber:  _number5
-              ),
-                             verificationInput.verificationInput(
-               inputNumber:  _number6
-              ),
-            ],
+                verificationInput.verificationInput(inputNumber: _number1),
+                verificationInput.verificationInput(inputNumber: _number2),
+                verificationInput.verificationInput(inputNumber: _number3),
+                verificationInput.verificationInput(inputNumber: _number4),
+                verificationInput.verificationInput(inputNumber: _number5),
+                verificationInput.verificationInput(inputNumber: _number6),
+              ],
             ),
-              SizedBox(height: 20,),
-             Text(
+            SizedBox(
+              height: 20,
+            ),
+            Text(
               'Enter 6 Digit Code',
-             style: TextStyle(
-              color:  Color(0xFFE0333333),
-              fontSize: 17,
-              fontWeight: FontWeight.w500,
-            ), 
-             ),
-
-            SizedBox(height: 50,),
-            RichText(
-              text: TextSpan(
-             text:  'Didn\'t receive code?',
               style: TextStyle(
-              color:  Color(0xFFE0333333),
-              fontSize: 13,
-              fontWeight: FontWeight.w400,
-            ), 
-            children: [
-            
-              TextSpan(
-            text: ' Resend Code',
-            style: TextStyle(
-              color:   Color(0xFFE0f3cd5b),
-              fontSize: 13,
-              fontWeight: FontWeight.w400,
+                color: Color(0xFFE0333333),
+                fontSize: 17,
+                fontWeight: FontWeight.w500,
+              ),
             ),
-            recognizer: TapGestureRecognizer()
-            ..onTap = () {
-            }   
-            
-
-          ),
-            ]
-             )
-             
+            SizedBox(
+              height: 50,
             ),
-            const SizedBox(height: 200,),
-
+            RichText(
+                text: TextSpan(
+                    text: 'Didn\'t receive code?',
+                    style: TextStyle(
+                      color: Color(0xFFE0333333),
+                      fontSize: 13,
+                      fontWeight: FontWeight.w400,
+                    ),
+                    children: [
+                  TextSpan(
+                      text: ' Resend Code',
+                      style: TextStyle(
+                        color: Color(0xFFE0f3cd5b),
+                        fontSize: 13,
+                        fontWeight: FontWeight.w400,
+                      ),
+                      recognizer: TapGestureRecognizer()..onTap = () {}),
+                ])),
+            const SizedBox(
+              height: 200,
+            ),
             GestureDetector(
-              onTap: (){
-                          context;
-             Navigator.push(
-             context,
-             MaterialPageRoute(builder: (context) => const HomePage()),
-            );
+              onTap: () {
+                context;
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const BasePage()),
+                );
               },
               child: SizedBox(
                 width: 200,
                 child: Image.asset(
                   'assets/button2.png',
-
                 ),
               ),
             )
           ],
         ),
-        ),
-      );
-    }
+      ),
+    );
+  }
 }
